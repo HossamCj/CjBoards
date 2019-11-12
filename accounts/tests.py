d1fbc4863745b90282fdd4a8d119b1cb8c792035
HossamCj <hossamcj@gmail.com>
2019-11-12 04:20:21 +0200

    create SignUP class test and checking status_code

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from .views import signup
+
+
+class SignUpTests(TestCase):
+    def test_signup_status_code(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
