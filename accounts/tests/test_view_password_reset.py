b24a0793566ef3a6c3b88c502af081bd0e3ffbec
HossamCj <hossamcj@gmail.com>
2019-11-20 20:14:52 +0200

    added tests for successful password reset

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.core import mail
 from django.test import TestCase
 from django.urls import reverse, resolve
 from django.contrib.auth import views as auth_views
@@ -36,5 +38,23 @@ class PasswordResetTest(TestCase):
         self.assertContains(self.response, 'type="email"', 1)
 
 
-    
+class SuccessfulPasswordResetTests(TestCase):
+    def setUp(self):
+        email = 'n3n3@doe.com'
+        User.objects.create_user(username='john', email=email, password='0110n3n3')
+        url = reverse('password_reset')
+        self.response = self.client.post(url, {'email': email})
 
+
+    def test_redirection(self):
+        '''
+        A valid form submission should redirect the user to `password_reset_done` view
+        '''
+        url = reverse('password_reset_done')
+        self.assertRedirects(self.response, url)
+
+
+    def test_send_password_reset_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    
