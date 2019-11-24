32accbbf8390e498b2a53807c0f769a0c25f6d4f
HossamCj <hossamcj@gmail.com>
2019-11-24 21:32:00 +0200

    created Reply_topic: view, url, template, tests, form

---
+++ boards/tests/test_view_reply_topic.py
@@ -0,0 +1,115 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+from ..forms import PostForm
+from ..views import reply_topic
+from ..models import Board, Topic, Post
+
+
+class ReplyTopicTestCase(TestCase):
+    '''
+    Base test case to be used in all 'reply_topic' view tests
+    '''
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='This is Django discussion')
+        self.username = 'bibo'
+        self.password = '01110abc'
+        user = User.objects.create_user(username=self.username, email='bibo@email.com', password=self.password)
+        self.topic = Topic.objects.create(subject='First topic!', board=self.board, starter=user)
+        Post.objects.create(message='Lorem ipsum dolor sit amet', topic=self.topic, created_by=user)
+        self.url = reverse('reply_topic', kwargs={'pk': self.board.pk, 'topic_pk': self.topic.pk})
+
+
+class LoginRequiredReplyTopicTests(ReplyTopicTestCase):
+    def test_redirection(self):
+        login_url = reverse('login')
+        response = self.client.get(self.url)
+        self.assertRedirects(response, '{login_url}?next={url}'.format(login_url=login_url, url=self.url))
+
+
+class ReplyTopicTests(ReplyTopicTestCase):
+    def setUp(self):
+        super().setUp()
+        self.client.login(username=self.username, password=self.password)
+        self.response = self.client.get(self.url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/boards/1/topics/1/reply/')
+        self.assertEquals(view.func, reply_topic)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, PostForm)
+
+    def test_form_inputs(self):
+        '''
+        The view should have 2 inputs: csrf and message textarea
+        '''
+        self.assertContains(self.response, '<input', 1)
+        self.assertContains(self.response, '<textarea', 1)
+
+
+class SuccessfulReplyTopicTests(ReplyTopicTestCase):
+    def setUp(self):
+        super().setUp()
+        self.client.login(username=self.username, password=self.password)
+        self.response = self.client.post(self.url, {'message': 'Hello, World'})
+
+    def test_redirection(self):
+        '''
+        The valid form submission should redirect the user
+        '''
+        topic_posts_url = reverse('topic_posts', kwargs={'pk': self.board.pk, 'topic_pk': self.topic.pk})
+        self.assertRedirects(self.response, topic_posts_url)
+
+    def test_reply_created_counter(self):
+        '''
+        The total post count should be 2
+        Th one created in the 'ReplyTopicTestCase' setUp (The first one)
+        and another created by the post data in this class
+        '''
+        self.assertEquals(Post.objects.count(), 2)
+
+
+class InvalidReplyTopicTests(ReplyTopicTestCase):
+    def setUp(self):
+        '''
+            Submit and empty dictionary to the 'reply_topic' view
+            '''
+        super().setUp()
+        self.client.login(username=self.username, password=self.password)
+        self.response = self.client.post(self.url, {})
+
+    def test_status_code(self):
+        '''
+        An invalid form submission should return the same page
+        '''
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- boards/views.py
+++ boards/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import NewTopicForm
+from .forms import NewTopicForm, PostForm
 from .models import Board, Post, Topic
 
 
@@ -10,11 +10,9 @@ def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
 
-
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
-
 
 @login_required
 def new_topic(request, pk):
@@ -31,12 +29,26 @@ def new_topic(request, pk):
                 topic=topic,
                 created_by=request.user
             )
-            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+            return redirect('board_topics', pk=board.pk)
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 
-
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
     return render(request, 'topic_posts.html', {'topic': topic})
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form = PostForm()
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
